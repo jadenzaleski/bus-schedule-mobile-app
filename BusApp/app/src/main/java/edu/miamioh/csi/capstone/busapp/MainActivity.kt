b25084d48eebc21a312aa59e9fc5ff842607c399
Jaden Zaleski <jadenzaleski@icloud.com>
2023-10-17 19:18:51 -0400

    added android project

---
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package edu.miamioh.csi.capstone.busapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.miamioh.csi.capstone.busapp.ui.theme.BusAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Jaden Zaleski")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BusAppTheme {
+        Greeting("Android")
+    }
+}
