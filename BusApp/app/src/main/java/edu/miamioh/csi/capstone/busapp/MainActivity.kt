bba8f68838cebeb9ccee8da3127e169ee1472ea2
Jaden Zaleski <jadenzaleski@icloud.com>
2023-10-19 01:09:18 -0400

    Added bottom tab view and created basic layout of the project. Commented all code. Build succeeded.

--- BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/MainActivity.kt
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/MainActivity.kt
@@ -3,13 +3,6 @@ package edu.miamioh.csi.capstone.busapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import edu.miamioh.csi.capstone.busapp.ui.theme.BusAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +10,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BusAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Jaden Zaleski")
-                }
+                // Main view of the app
+                MainView()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BusAppTheme {
-        Greeting("Android")
     }
 }
