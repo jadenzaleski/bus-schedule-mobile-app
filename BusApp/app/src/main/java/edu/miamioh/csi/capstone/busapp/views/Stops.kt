c59224c2b6dcabf28609fcf2356b85b91d0d2424
Jaden Zaleski <jadenzaleski@icloud.com>
2023-11-28 19:43:08 -0500

    Customized the map to use the custom maps that we designed in mapbox.

--- BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Stops.kt
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Stops.kt
@@ -1,10 +1,13 @@
 package edu.miamioh.csi.capstone.busapp.views
 
+import android.content.res.Resources.Theme
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.mapbox.common.MapboxOptions
 import com.mapbox.geojson.Point
@@ -15,6 +18,8 @@ import com.mapbox.maps.Style
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.annotation.generated.CircleAnnotation
 import edu.miamioh.csi.capstone.busapp.CSVHandler
+import edu.miamioh.csi.capstone.busapp.ui.theme.isDark
+
 
 @OptIn(MapboxExperimental::class)
 @Composable
@@ -24,8 +29,8 @@ fun StopsView() {
         mapInitOptionsFactory = { context ->
             MapInitOptions(
                 context = context,
-
-                styleUri = Style.LIGHT,
+                // could aslo be: if(isDark) Style.DARK else Style.LIGHT
+                styleUri = if(isDark) "mapbox://styles/jadenzaleski/clpj08tiz00dh01qu8tv9fp25" else "mapbox://styles/jadenzaleski/clpj1cq5200dj01qmdiarhnwa",
                 cameraOptions = CameraOptions.Builder()
                     .center(Point.fromLngLat(16.5952, 38.9048))
                     .zoom(9.0)
