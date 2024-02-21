ee8efd27a67cf8d49e7674ab3ffa75b430b26c37
ayo obisesan <77123546+Ayo-SE@users.noreply.github.com>
2024-02-20 19:33:47 -0500

    Demo Implementation of a Map in the Stops.kt file

--- BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Stops.kt
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Stops.kt
@@ -3,30 +3,65 @@ package edu.miamioh.csi.capstone.busapp.views
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import edu.miamioh.csi.capstone.busapp.CSVHandler
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun StopsView() {
+    val singapore = LatLng(1.35, 103.87)
+    val singaporeState = MarkerState(position = singapore)
+    val sydney = LatLng(-33.867, 151.207) // Assuming you have a Sydney LatLng defined somewhere
+    val sydneyState = MarkerState(position = sydney)
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Blue),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = CSVHandler.getInfo().toString(),
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = singaporeState,
+                title = "Marker in Singapore",
+            )
+            Marker(
+                state = sydneyState,
+                title = "Marker in Sydney"
+            )
+        }
     }
 }
+//@Composable
+//fun StopsView() {
+//    Box(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color.Blue),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        Text(
+//            text = CSVHandler.getInfo().toString(),
+//            fontWeight = FontWeight.Bold,
+//            color = Color.White
+//        )
+//    }
+//}
 
 @Composable
 @Preview
