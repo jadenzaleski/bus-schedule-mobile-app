80942a713d1da172d058cc8d8ec1e0a31b25b614
ayo obisesan <77123546+Ayo-SE@users.noreply.github.com>
2024-02-27 01:05:19 -0500

    No crash on the current location button, passes MVP requirement for the feature

--- BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Stops.kt
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Stops.kt
@@ -1,5 +1,7 @@
 package edu.miamioh.csi.capstone.busapp.views
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,6 +12,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -24,13 +27,25 @@ import edu.miamioh.csi.capstone.busapp.CSVHandler
 fun StopsView() {
     val stops = CSVHandler.getStops()
     val context = LocalContext.current
-    val uiSettings = remember {
-        MapUiSettings(myLocationButtonEnabled = true)
-    }
+//    val uiSettings = remember {
+//        MapUiSettings(myLocationButtonEnabled = true)
+//    }
     var isLocationPermissionGranted by remember { mutableStateOf(false) }
+//    var showPermissionRationale by remember { mutableStateOf(false) }
 
+    // Prepare the permission launcher
+//    val requestPermissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+//        if (isGranted) {
+//            isLocationPermissionGranted = true
+//        } else {
+//            isLocationPermissionGranted = false
+//        }
+//    }
+
+    // This block of code checks to see if the locationPermission has been granted beforehand
     LaunchedEffect(key1 = context) {
-        isLocationPermissionGranted = true
+        isLocationPermissionGranted = ContextCompat.checkSelfPermission(
+            context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 
     val initialPosition = LatLng(38.9048, 16.5952)
@@ -38,10 +53,32 @@ fun StopsView() {
         position = CameraPosition.fromLatLngZoom(initialPosition, 9f)
     }
 
+//    if (showPermissionRationale) {
+//        // Show a dialog explaining why the permission is needed
+//        AlertDialog(
+//            onDismissRequest = { showPermissionRationale = false },
+//            title = { Text("Location Permission Required") },
+//            text = { Text("This feature requires location permissions to function. Please grant them.") },
+//            confirmButton = {
+//                Button(onClick = {
+//                    showPermissionRationale = false
+//                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+//                }) {
+//                    Text("OK")
+//                }
+//            },
+//            dismissButton = {
+//                Button(onClick = { showPermissionRationale = false }) {
+//                    Text("Cancel")
+//                }
+//            }
+//        )
+//    }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
-        uiSettings = uiSettings,
+        uiSettings = MapUiSettings(myLocationButtonEnabled = isLocationPermissionGranted, compassEnabled = true),
         properties = MapProperties(isMyLocationEnabled = isLocationPermissionGranted)
     ) {
         val stopCount = minOf(stops.size, 50)
