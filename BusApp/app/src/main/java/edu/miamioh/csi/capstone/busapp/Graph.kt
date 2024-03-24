35924b7391a9fa41585ef52f65065538ad939e4f
Daniel Tai <dtai.cs@gmail.com>
2024-03-24 00:33:08 -0400

    Added some comments for code readability.

--- BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/Graph.kt
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/Graph.kt
@@ -1,12 +1,14 @@
 package edu.miamioh.csi.capstone.busapp
 
 object Graph {
-
     /**
-     * Based on the list of user-selected agencies, returns the list
-     * of ALL StopIds associated with those agencies.
+     * Based on the list of user-selected agencies, returns the list of ALL StopIds associated
+     * with those agencies. Funnels through manually created associations between required .csv
+     * files in order to acquire final list of StopIds to be returned.
      *
-     * @param agencyIdList The list of active agencies selected by the user
+     * @param agencyIdList The list of active agencies selected by the user, represented by their
+     *                     ID number.
+     * @return a list of all StopIds that are associated with the agencies selected
      */
     fun findAllValidStopIdByAgencyId(
         agencyIdList: Set<Int>,
@@ -16,18 +18,19 @@ object Graph {
         trips: List<Trip>,
         stopTimes: List<StopTime>
     ): Set<Int> {
-        // Filter routes by agency_id
+        // Find all qualifying routes based on the list of agencyIDs provided.
         val filteredRoutes = routes.filter { it.agencyID in agencyIdList }
 
-        // Use filtered routes to find corresponding trips
+        // Find all qualifying trips based on the qualifying routes previously found.
         val filteredTripIds = trips.filter { trip ->
             filteredRoutes.any { it.routeID == trip.routeID }
         }.map { it.tripID }.toSet()
 
-        // Use those trips to find stop_ids in StopTimes
+        // Find all qualifying stops (represented by ID) based on the qualifying trips previously
+        // found.
         val stopIds = stopTimes.filter { it.tripID in filteredTripIds }.map { it.stopID }.toSet()
 
-        // Return the set of stop_ids without duplicates
+        // Return the generated set of Stop IDs. Since this is a set, there are no duplicates.
         return stopIds
     }
 }
