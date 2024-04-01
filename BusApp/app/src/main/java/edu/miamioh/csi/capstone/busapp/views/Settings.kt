a317c858d9e44b2f6a6280f1b8bbd65c10181d0a
Daniel Tai <dtai.cs@gmail.com>
2024-04-01 00:46:34 -0400

    Fixing commit status issue: Settings page progress now matches develop (which was the main branch this one was created from.

--- BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Settings.kt
+++ BusApp/app/src/main/java/edu/miamioh/csi/capstone/busapp/views/Settings.kt
@@ -3,7 +3,6 @@ package edu.miamioh.csi.capstone.busapp.views
 import android.app.Activity
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -16,8 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -28,11 +25,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,23 +34,11 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import edu.miamioh.csi.capstone.busapp.UnzipUtils
-import edu.miamioh.csi.capstone.busapp.backend.CSVHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import me.zhanghai.compose.preference.ProvidePreferenceLocals
 import me.zhanghai.compose.preference.footerPreference
 import me.zhanghai.compose.preference.preference
 import me.zhanghai.compose.preference.sliderPreference
 import me.zhanghai.compose.preference.switchPreference
-import java.io.File
-import java.io.FileInputStream
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-
 
 @Composable
 fun SettingsView() {
@@ -68,112 +48,8 @@ fun SettingsView() {
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun SettingScreen() {
-    val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-
     val backgroundColor = MaterialTheme.colorScheme.background
     val onBackgroundColor = MaterialTheme.colorScheme.onBackground
-
-    // State to control the visibility of the update data dialog
-    var showUpdateDataAlert by remember { mutableStateOf(false) }
-    var isUpdating by remember { mutableStateOf(false) }
-    var lastUpdateTime by remember { mutableStateOf("No manual updates triggered since last full app refresh") }
-
-    // Show progress dialog based on isUpdating state
-    UpdateProgressDialog(isVisible = isUpdating)
-
-    // Dialog showing logic
-    if (showUpdateDataAlert) {
-        AlertDialog(
-            onDismissRequest = {
-                // Handle the dismiss action here
-                showUpdateDataAlert = false
-            },
-            confirmButton = {
-                Button(
-                    onClick = {
-                        showUpdateDataAlert = false // Close the dialog
-                        isUpdating = true // Begin updating
-
-                        coroutineScope.launch {
-                            try {
-                                // Perform operations in IO dispatcher
-                                withContext(Dispatchers.IO) {
-                                    val url = "https://mobilita.regione.calabria.it/gtfs/otp_gtfs.zip"
-                                    val destinationFile = File(context.getExternalFilesDir(null), "otp_gtfs.zip")
-
-                                    // Ensure the directory for extracted files is clear
-                                    val coreDir = File(context.getExternalFilesDir(null), "core")
-                                    if (deleteDirectoryWithContents(coreDir)) {
-                                        Log.i("DELETE", "Previous data deleted successfully.")
-                                    }
-
-                                    // Recreate directories
-                                    if (!coreDir.exists()) {
-                                        coreDir.mkdirs()
-                                    }
-
-                                    // Replace this with the actual download logic
-                                    CSVHandler.downloadFile(url, destinationFile)
-                                    Log.i("DOWNLOAD", "Download job is complete (Stage 1).")
-
-                                    // Add your other operations here
-                                    // Ensure you replace the placeholder operations with actual implementation
-                                    UnzipUtils.unzip(destinationFile, context.getExternalFilesDir(null)?.absolutePath + "/core/")
-                                    Log.i("UNZIP", "Unzip job is complete (Stage 2).")
-
-                                    CSVHandler.renameToCSV(context.getExternalFilesDir(null)?.absolutePath + "/core")
-                                    Log.i("RENAME", "Rename Job is complete (Stage 3).")
-
-                                    // Initialize CSVHandler with the correct paths
-                                    withContext(Dispatchers.IO) {
-                                        CSVHandler.initialize(
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/agency.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/calendar.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/calendar_dates.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/feed_info.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/routes.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/stop_times.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/stops.csv"),
-                                            FileInputStream("/storage/emulated/0/Android/data/edu.miamioh.csi.capstone.busapp/files/core/trips.csv")
-                                        )
-                                    }
-
-                                    Log.i("CSV", "CSV initialization complete!")
-
-                                    // Simulate long-running task
-                                    delay(3000) // Remember to import kotlinx.coroutines.delay
-
-                                    // Update last update time on UI thread after success
-                                    withContext(Dispatchers.Main) {
-                                        val dateFormat = SimpleDateFormat("MMM dd, yyyy HH:mm:ss", Locale.getDefault())
-                                        lastUpdateTime = "${dateFormat.format(Date())}"
-                                        isUpdating = false
-                                    }
-                                }
-                            } catch (e: Exception) {
-                                Log.e("MainActivity", "Error in downloading or processing files: ${e.message}")
-                            } catch (e: Exception) {
-                                Log.e("UpdateError", "Error during update: ${e.message}")
-                            } finally {
-                                isUpdating = false // End updating
-                            }
-                        }
-                    }
-                ) {
-                    Text("Confirm")
-                }
-            },
-            dismissButton = {
-                Button(onClick = { showUpdateDataAlert = false }) {
-                    Text("Cancel")
-                }
-            },
-            title = { Text(text = "Confirm Action") },
-            text = { Text(text = "Do you want to update the data now? Depending on your current"
-                + " network speed, this action may take a couple of minutes.") }
-        )
-    }
 
     Column(
         modifier = Modifier
@@ -226,12 +102,8 @@ fun SettingScreen() {
             preference(
                 key = "updateData",
                 title = { Text(text = "Update Data") },
-                summary = { Text(text = "Last Update: $lastUpdateTime" ) },
-                onClick = {
-                    // When the preference is clicked, show the dialog
-                    showUpdateDataAlert = true
-                }
-            )
+                summary = { Text(text = "Last Update: 4:00 AM") }
+            ) {}
             preference(
                 key = "about",
                 title = { Text(text = "About/App Info") },
@@ -287,36 +159,6 @@ fun SetTheme(
 
     MaterialTheme(colorScheme = colorScheme, content = content)
 }
-
-@Composable
-fun UpdateProgressDialog(isVisible: Boolean) {
-    if (isVisible) {
-        AlertDialog(
-            onDismissRequest = { /* Disallow dismissing while updating */ },
-            title = { Text("Updating Now") },
-            text = { Text("Retrieving most recent bus data from CORe, please wait...") },
-            confirmButton = {},
-            dismissButton = {}
-        )
-    }
-}
-
-fun deleteDirectoryWithContents(directory: File): Boolean {
-    if (directory.exists()) {
-        val files = directory.listFiles()
-        if (files != null) {
-            for (file in files) {
-                if (file.isDirectory) {
-                    deleteDirectoryWithContents(file)
-                } else {
-                    file.delete()
-                }
-            }
-        }
-    }
-    return directory.delete()
-}
-
 
 @Composable
 @Preview(showBackground = true)
